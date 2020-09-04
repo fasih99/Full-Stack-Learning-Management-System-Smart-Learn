f22c2858a474f58c81d188b635a6e8a3cd7e571a
fasih99 <fasih.hussaini99@gmail.com>
2020-09-04 22:14:40 +0530

    Whitenoise Added

--- elearn/settings.py
+++ elearn/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -142,6 +143,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
